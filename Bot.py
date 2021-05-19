b485c4f780ae2162f9ed0882c9cbd5f596409713
xXACRVXx <70526454+xXACRVXx@users.noreply.github.com>
2021-05-19 19:02:18 -0400

    update Bot.py

--- Bot.py
+++ Bot.py
@@ -1,1 +1,22 @@
+from telegram.ext import Updater, CommandHandler
+
+def start(update, context):
+      update.message.reply_text('Hola, humano')
+      
+def help(update, context):
+         update.message.reply_text('por ahora solo dice Hola humano cuando escribes /start')
+
+if __name__ == '__main__':
+    
+     updater =        Updater(token='1756188628:AAG6uWqj8VKDxJsiajTCq8mrsq6S3hWgOEQ', use_context=True)
+     
+     dp = updater.dispatcher
+    
+     dp.add_handler(CommandHandler('start', start))
+     
+     dp.add_handler(CommandHandler('help', help))
+     
+     updater.start_polling()
+      
+     updater.idle()
 
