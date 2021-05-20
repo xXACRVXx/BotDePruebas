6cf9455c25fc8016158fd5a5db9ef06e23d4992d
xXACRVXx <70526454+xXACRVXx@users.noreply.github.com>
2021-05-19 21:39:17 -0400

    Rename Bot.py to Bot.py

---
+++ Bot.py
@@ -0,0 +1,22 @@
+from telegram.ext import Updater, CommandHandler
+
+def start(update, context):
+      update.message.reply_text('Hola, humano')
+      
+def help(update, context):
+         update.message.reply_text('por ahora solo dice Hola humano cuando escribes /start')
+
+if __name__ == '__main__':
+    
+     updater =        Updater(token='you token', use_context=True)
+     
+     dp = updater.dispatcher
+    
+     dp.add_handler(CommandHandler('start', start))
+     
+     dp.add_handler(CommandHandler('help', help))
+     
+     updater.start_polling()
+      
+     updater.idle()
+
